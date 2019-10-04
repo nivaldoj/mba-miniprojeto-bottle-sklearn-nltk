cbe06632a7d662bf39e13e5a7701e41c930a3136
Nivaldo Carvalho <nivaldo_j@outlook.com>
2019-10-04 11:33:01 -0300

    Constant para o caminho do projeto no PythonAnywhere

--- bottle_app.py
+++ bottle_app.py
@@ -14,17 +14,20 @@ from bottle import run
 from sklearn.naive_bayes import GaussianNB
 import joblib
 
+# caminho da pasta do projeto no servidor
+PASTA_PROJETO = "/home/projetonmcj/mysite/"
+
 
 @get('/')
 def index():
     # renderiza a página inicial do projeto
-    return template("index.html")
+    return template(PASTA_PROJETO + "index.html")
 
 
 @get('/projeto_mamiferos')
 def mamiferos_get():
     # renderiza o formulário de classificação de mamíferos
-    return template("forms/form_mamifero.html", animal="-", classificacao="-", probabilidade="-")
+    return template(PASTA_PROJETO + "forms/form_mamifero.html", animal="-", classificacao="-", probabilidade="-")
 
 
 @post('/projeto_mamiferos')
@@ -38,7 +41,7 @@ def mamiferos_post():
 
     # carrega o modelo
     modelo_nb = GaussianNB()
-    modelo_nb = joblib.load('models/model_mamifero.pkl')
+    modelo_nb = joblib.load(PASTA_PROJETO + "models/model_mamifero.pkl")
 
     # executa a classificação
     res = modelo_nb.predict([[sangue, bota_ovo, voa, mora_agua]])
@@ -54,13 +57,13 @@ def mamiferos_post():
         clf = "Indefinido"
 
     # renderiza o template com os resultados
-    return template("forms/form_mamifero.html", animal=animal, classificacao=clf, probabilidade=prb)
+    return template(PASTA_PROJETO + "forms/form_mamifero.html", animal=animal, classificacao=clf, probabilidade=prb)
 
 
 @get('/projeto_credito')
 def credito_get():
     # renderiza o formulário de classificação de crédito
-    return template("forms/form_credito.html", risco="-", renda="-", probabilidade="-")
+    return template(PASTA_PROJETO + "forms/form_credito.html", risco="-", renda="-", probabilidade="-")
 
 
 @post('/projeto_credito')
@@ -74,7 +77,7 @@ def credito_post():
 
     # carrega o modelo
     modelo_nb = GaussianNB()
-    modelo_nb = joblib.load('models/model_credito.pkl')
+    modelo_nb = joblib.load(PASTA_PROJETO + "models/model_credito.pkl")
 
     # executa a classificação
     res = modelo_nb.predict([[renda, credito, divida, garantias]])
@@ -90,7 +93,7 @@ def credito_post():
         clf = "Indefinido"
 
     # renderiza o template com os resultados
-    return template("forms/form_credito.html", risco=clf, renda=renda, probabilidade=prb)
+    return template(PASTA_PROJETO + "forms/form_credito.html", risco=clf, renda=renda, probabilidade=prb)
 
 
 # executa a aplicação bottle
