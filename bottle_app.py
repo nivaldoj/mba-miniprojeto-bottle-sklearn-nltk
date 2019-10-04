4875da48e7c2bd7d3178b5e0e45cc45032f9137f
Nivaldo Carvalho <nivaldo_j@outlook.com>
2019-10-04 11:28:54 -0300

    Adicionado código para classificação de crédito

--- bottle_app.py
+++ bottle_app.py
@@ -53,12 +53,48 @@ def mamiferos_post():
     else:
         clf = "Indefinido"
 
-    # renderiza o template
-    return template('forms/form_mamifero.html', animal=animal, classificacao=clf, probabilidade=prb)
+    # renderiza o template com os resultados
+    return template("forms/form_mamifero.html", animal=animal, classificacao=clf, probabilidade=prb)
+
+
+@get('/projeto_credito')
+def credito_get():
+    # renderiza o formulário de classificação de crédito
+    return template("forms/form_credito.html", risco="-", renda="-", probabilidade="-")
+
+
+@post('/projeto_credito')
+def credito_post():
+    # obtêm valores informados no formulário
+    pessoa = request.forms.get('pessoa')
+    renda = float(request.forms.get('renda'))
+    credito = int(request.forms.get('credito'))
+    divida = int(request.forms.get('divida'))
+    garantias = int(request.forms.get('garantias'))
+
+    # carrega o modelo
+    modelo_nb = GaussianNB()
+    modelo_nb = joblib.load('models/model_credito.pkl')
+
+    # executa a classificação
+    res = modelo_nb.predict([[renda, credito, divida, garantias]])
+
+    # encontra o valor da confidência
+    prb = modelo_nb.predict_proba([[renda, credito, divida, garantias]])
+
+    if res == 1:
+        clf = "Alto"
+    elif res == 0:
+        clf = "Medio"
+    else:
+        clf = "Indefinido"
+
+    # renderiza o template com os resultados
+    return template("forms/form_credito.html", risco=clf, renda=renda, probabilidade=prb)
 
 
 # executa a aplicação bottle
 application = default_app()
 
 # necessário para executar localmente
-#run(application, host="localhost", port=80)
+run(application, host="localhost", port=80)
